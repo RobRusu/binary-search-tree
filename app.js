78d299808095c521ff87c088c6086b89aab6a1e9
RobRusu <142660890+RobRusu@users.noreply.github.com>
2024-06-26 12:31:03 +0100

    cleared CLI testing part

--- app.js
+++ app.js
@@ -427,32 +427,3 @@ class Tree {
     }
   }
 }
-
-//testing in CLI
-const projectArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const practiceArray = [50];
-
-const tree = new Tree(projectArray);
-
-// print a structured tree to visualize the BST easier
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-  if (node === null) {
-    return;
-  }
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-  }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-  }
-};
-
-tree.insert(19);
-tree.insert(17);
-console.log(prettyPrint(tree.root));
-
-const node = tree.find(8);
-console.log(tree.isBalanced());
-console.log(tree.rebalance());
-console.log(prettyPrint(tree.root));
